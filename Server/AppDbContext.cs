5917fd3cfd5a9a861eb0ac60a601d807b3e7ad2e
Gabriel Henrique <gabrielhenrique9981@gmail.com>
2020-09-29 23:35:27 -0300

    Crud Completo fé

---
+++ Server/AppDbContext.cs
@@ -0,0 +1,14 @@
+using crudBlazor.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace crudBlazor.Server
+{
+     public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
